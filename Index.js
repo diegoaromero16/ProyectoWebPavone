0c16e212c3cb8fcf17d5d0d4b1f457f305df6506
Diego Romero <diegoaromero16@gmail.com>
2023-08-21 22:02:04 -0300

    Se arregla la navbar, se agregan animaciones, resta comenzar el diseño del index

--- Index.js
+++ Index.js
@@ -1,10 +1,23 @@
-const textoAnimado = document.querySelector('.texto-animado');
-
-window.addEventListener('scroll', () => {
-  const imageTop = document.querySelector('img').getBoundingClientRect().top;
-  const viewportBottom = window.innerHeight;
-
-  if (imageTop < viewportBottom) {
-    textoAnimado.classList.add('animate-text');
+// window.addEventListener("scroll", function() {
+//   var navbar = document.getElementById("navbar-style");
+//   if (window.scrollY > 50) {
+//       navbar.classList.add("navbar-scrolled");
+//       navbar.classList.add("navbar-smaller");
+//   } else {
+//       navbar.classList.remove("navbar-scrolled");
+//       navbar.classList.remove("navbar-smaller");
+//   }
+// });
+window.addEventListener("scroll", function () {
+  var logoNavbar = document.getElementById("logoimg");
+  var header = document.getElementById("headernav");
+  if (window.scrollY > 50) {
+    logoNavbar.classList.add("Image-with-navbar-scrolled");
+    header.classList.add("nav-small");
+  } else {
+    logoNavbar.classList.remove("Image-with-navbar-scrolled");
+    header.classList.remove("nav-small");
   }
 });
+
+
